d60abe79ae29ee2b8f61f446e84987bd4aba666b
Tanner Linsley <tannerlinsley@gmail.com>
2025-05-15 09:54:29 -0600

    checkpoint

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,7 @@ import {
   createStartHandler,
   defaultStreamHandler,
 } from '@tanstack/react-start/server'
-import { getRouterManifest } from '@tanstack/react-start/router-manifest'
 
 import { createRouter } from './router'
 
-export default createStartHandler({
-  createRouter,
-  getRouterManifest,
-})(defaultStreamHandler)
+export default createStartHandler({ createRouter })(defaultStreamHandler)
